b59598ab73638eef2d8866e899abc4cba5b89bd0
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-11-16 19:30:37 -0300

    Projeto Criado!

---
+++ back/src/test/java/com/shorthis/ShortHis/ShortHisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shorthis.ShortHis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShortHisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
