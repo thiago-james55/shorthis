777c4db4055a680fe784dab8cdfab065081f521a
Thiago Gonçalves Souza <tsyhwh55@gmail.com>
2022-12-03 20:42:11 -0300

    Removendo imports nao usados

--- back/src/main/java/com/shorthis/service/exception/handler/SQLExceptionHandler.java
+++ back/src/main/java/com/shorthis/service/exception/handler/SQLExceptionHandler.java
@@ -1,17 +1,10 @@
 package com.shorthis.service.exception.handler;
 
-import com.shorthis.service.exception.ShortedUrlNotFoundException;
-import com.shorthis.service.exception.UrlException;
-import com.shorthis.service.exception.UserException;
-import com.shorthis.service.exception.UserNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.context.MessageSource;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -19,8 +12,7 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 
 import java.sql.SQLIntegrityConstraintViolationException;
 import java.time.OffsetDateTime;
-import java.util.ArrayList;
-import java.util.List;
+;
 
 @AllArgsConstructor
 @ControllerAdvice
